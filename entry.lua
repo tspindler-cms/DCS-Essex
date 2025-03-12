c2284838a71df38fac062e7c7f164a68846ad643
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-03-12 03:35:06 +0100

    Update of CVA-31 by James
        - Add guns
        - Change runway to align with model
        - Fix various model mistakes
    	- Fix model gaps and bad mapping
            - Fix radar rotate direction
            - New and functional FCS director
            - New JBD animation
            - Fix propeller position
        - Change GT name to use by weapon sounds
        - Add new generic Essex liveries
        - Remove CVA-9 Essex
        - Change top speed setting

--- entry.lua
+++ entry.lua
@@ -3,7 +3,7 @@ declare_plugin("tetet_cva31",
 	dirName		  = current_mod_path,
 	displayName   = _("CVA-31 Bon Homme Richard"),
 	shortName	  =   "CVA-31",
-	version		  =   "0.8.0",
+	version		  =   "0.10.0",
 	state		  =   "installed",
 	fileMenuName = _("vwv_cva31"),
 	developerName =   "TeTeT",
@@ -24,6 +24,7 @@ mount_vfs_liveries_path (current_mod_path .. "/Liveries")
 mount_vfs_model_path    (current_mod_path .. "/Shapes")
 mount_vfs_texture_path	(current_mod_path .. "/Textures/cva-31.zip")
 
+dofile(current_mod_path..'/Weapons/CVA-31_Ammo.lua') -- any custom weapons the mod has
 dofile(current_mod_path .. "/Database/db_ships.lua")
 
 plugin_done()
